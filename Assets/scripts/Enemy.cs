ca978202984afc0060ceb7d8441fc0082822f830
Sergi <smaymi@portic.net>
2020-01-13 19:30:53 +0100

    init

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(Random.Range(-14f, 14f), 11, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit: " + other.transform.name);
+    }
+}
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2;
+    [SerializeField]
+    private GameObject laserPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserPrefab, transform.position, Quaternion.identity);
+        }
+
+
+    }
+
+    private void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (transform.position.y < 1)
+        {
+            transform.position = new Vector3(transform.position.x, 1f, 0);
+        }
+        else if (transform.position.y > 9.8)
+        {
+            transform.position = new Vector3(transform.position.x, 9.8f, 0);
+        }
+        if (transform.position.x < -14)
+        {
+            transform.position = new Vector3(14, transform.position.y, 0);
+        }
+        else if (transform.position.x > 14)
+        {
+            transform.position = new Vector3(-14, transform.position.y, 0);
+        }
+    }
+}
